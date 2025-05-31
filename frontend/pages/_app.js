1f5a785756fdc8b34b937f3613c1cc54d2709fb1
chojuninengu <chojuninengu@gmail.com>
2025-05-31 18:05:12 +0100

    Add layout and toast notifications to main application component. Integrate global CSS styles and wrap page components with a consistent layout for improved user experience.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,1 +1,14 @@
- 
+import { Toaster } from 'react-hot-toast';
+import Layout from '../components/Layout';
+import '../styles/globals.css';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <Toaster position="top-right" />
+    </>
+  );
+} 
